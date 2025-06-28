9a23a79e4677582e7779ced67fb6be152eea8e91
Nate Schieber <nbschieber@gmail.com>
2025-06-27 19:41:07 -0700

    wip: music library, scaffold track

--- src/db/entity/musicLibrary/Track.h
+++ src/db/entity/musicLibrary/Track.h
@@ -6,19 +6,32 @@
 #define TRACK_H
 
 #include <string>
+#include <sndfile.h>
 
 #include <sqlite3.h>
+
+#include "../../../enums/AudioCodec.h"
 
 namespace Gj {
 namespace Db {
 
 struct Track {
-  int id;
+  uint64_t id;
   std::string filePath;
   std::string title;
-  int sf_frames;
+  uint64_t duration;
+  uint16_t trackNumber;
+  uint32_t bitRate;
+
+  sf_count_t sf_frames;
   int sf_samplerate;
   int sf_channels;
+  int sf_format;
+  int sf_sections;
+  int sf_seekable;
+
+  bool isLossless;
+  bool isVariableBitRate;
 
   Track(int id, const std::string& filePath, const std::string& title);
   static Track deser(sqlite3_stmt* stmt);
---
+++ src/enums/AudioCodec.h
@@ -0,0 +1,23 @@
+//
+// Created by ns on 6/27/25.
+//
+
+#ifndef AUDIOCODEC_H
+#define AUDIOCODEC_H
+
+namespace Gj {
+  enum AudioCodec {
+    WAV,
+    ALAC,
+    AAC,
+    FLAC,
+    OGG,
+    MP3_320,
+    MP3_256,
+    MP3_192,
+    MP3_VAR,
+    UNRECOGNIZED
+  };
+}
+
+#endif //AUDIOCODEC_H
