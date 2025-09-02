f7fc8fcaed1e754eb22ac6d2f0ea396367eb8d89
Nate Schieber <nbschieber@gmail.com>
2025-09-02 01:00:52 -0700

    wip: vu meter, scaffold

---
+++ src/gui/Mixer/Channel/EffectsChannel/VuMeter.cpp
@@ -0,0 +1,13 @@
+//
+// Created by ns on 9/2/25.
+//
+
+#include "VuMeter.h"
+
+namespace Gj {
+namespace Gui {
+
+
+
+} // Gui
+} // Gj
---
+++ src/gui/Mixer/Channel/EffectsChannel/VuMeter.h
@@ -0,0 +1,24 @@
+//
+// Created by ns on 9/2/25.
+//
+
+#ifndef VUMETER_H
+#define VUMETER_H
+
+#include <QtOpenGLWidgets/QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <QtOpenGL/QOpenGLShaderProgram>
+
+namespace Gj {
+namespace Gui {
+
+class VuMeter : public QOpenGLWidget, protected QOpenGLFunctions {
+
+};
+
+} // Gui
+} // Gj
+
+
+
+#endif //VUMETER_H
