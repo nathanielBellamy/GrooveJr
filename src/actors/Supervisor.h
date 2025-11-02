fb2f98f3f6e08328c27d38eee277d769dd9d106c
Nate Schieber <nbschieber@gmail.com>
2025-11-01 22:39:41 -0700

    QApp threading note

--- src/actors/Supervisor.h
+++ src/actors/Supervisor.h
@@ -102,6 +102,16 @@ struct SupervisorState {
 
     // NOTE:
     // - QApplication::exec: Must be called from the main thread
+    // - *** Assertion failure in -[NSMenu _setMenuName:], NSMenu.m:777
+    //    *** Terminating app due to uncaught exception 'NSInternalInconsistencyException',
+    //    reason: 'API misuse: setting the main menu on a non-main thread.
+    //    Main menu contents should only be modified from the main thread.'
+    // - so no starting the QApp in the under the Display Actor
+    // - if ever we need more control:
+    // while (true) {
+    //   qtApp->processEvents();
+    //   std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    // }
     qtApp->exec();
   }
 
